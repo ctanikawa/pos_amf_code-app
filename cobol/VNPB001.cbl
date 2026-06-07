ee8f6cdef4fd5db830abbdfbc9e5d98cf241d59a
ctanikawa <162917388+ctanikawa@users.noreply.github.com>
2026-06-06 22:04:54 -0300

    alterado pgm 001 e criado 002

--- cobol/VNPB001.cbl
+++ cobol/VNPB001.cbl
@@ -1,3 +1,3 @@
-linha-develop-cobol-1
-linha-develop-cobol-2
-linha-develop-cobol-3
+linha-develop-cobol-1-1
+linha-develop-cobol-1-2
+linha-develop-cobol-1-3
---
+++ cobol/VNPB002.cbl
@@ -0,0 +1,3 @@
+linha-develop-cobol-2-1
+linha-develop-cobol-2-2
+linha-develop-cobol-2-3
